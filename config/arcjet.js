ef2e9bc12ea3962b50b46df0e018a1f3e21f87f6
shayaan-git <mshayaan24@gmail.com>
2025-07-10 22:34:57 +0530

    modified files

--- config/arcjet.js
+++ config/arcjet.js
@@ -1,11 +1,11 @@
-import arcjet, { shield, botV2, tokenBucket } from "@arcjet/node";
+import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
 import { ARCJET_KEY } from "./env.js";
 
 const aj = arcjet({
   key: ARCJET_KEY,
   rules: [
     shield({ mode: "LIVE" }),
-    botV2({
+    detectBot({
       mode: "LIVE",
       allow: [
         "CATEGORY:SEARCH_ENGINE",
